0489a3be68f87d421cf064a37ec85d10e2630429
Teklehaimanot Belete <enbish.blt@gmail.com>
2021-10-03 19:08:00 +0300

    added base url

--- src/sagas/requests/employee.ts
+++ src/sagas/requests/employee.ts
@@ -2,16 +2,16 @@ import axios from "axios";
 import { Action } from "../../state/action";
 import { employee } from "../../state/employeeReducer";
 
-const baseUrl = process.env.BASE_URL;
+const baseUrl = 'https://employeapi.herokuapp.com';
 
 export const requestGetEmployee = () =>axios.get<employee[]>(`${baseUrl}/api/employees`);
 
 export const requestDeleteEmployee =(action:Action)=>axios.delete(`${baseUrl}/api/employees/${action.payload}`)
 
-export const requestAddEmployee =(action:Action)=>axios.post(`${baseUrl}/`,action.payload)
+export const requestAddEmployee =(action:Action)=>axios.post(`${baseUrl}/api/employees`,action.payload)
 
 
 export const requestUpdateEmployee =(action:Action)=>{
 const act = Object.values(action.payload)
-axios.put(`${baseUrl}/${act[0]}`,act[1])
+axios.put(`${baseUrl}/api/employees/${act[0]}`,act[1])
 }
